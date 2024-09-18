1a8b17a8ef09603815c6560432d7ba7ee636dfa3
María Fernanda González <mafergonzalezw@gmail.com>
2024-09-18 20:23:00 +0000

    Listo

--- src/models.py
+++ src/models.py
@@ -1,32 +1,71 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class MyEnum(enum.Enum):
+    reels = 1
+    post = 2
+    historias = 3
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
+class User (Base):
+        __tablename__ = 'User'
+        id = Column(Integer, primary_key=True)
+        username = Column(String(250), nullable=False)
+        firstname = Column(String(250), nullable=False)
+        lastname = Column(String(250), nullable=False)
+        email= Column(String(250))
+
+        def to_dict(self):
+            return {}
+
+
+class Post(Base):
+        __tablename__ = 'Post'
+        id = Column(Integer, primary_key=True)
+        user_id = Column(Integer, ForeignKey('User.id'))
+
+        def to_dict(self):
+            return {}
+        
+        
+class Media(Base):
+      __tablename__ = "Media"
+      id = Column(Integer, primary_key=True)
+      typo = Column (Enum(MyEnum),nullable=False )
+      url = Column (String (250))
+      post_id = Column (Integer, ForeignKey('Post.id'))
+
+      def to_dict(self):
+            return {}
+      
+
+class Comment (Base):
+        __tablename__ = "Comment"
+        id = Column(Integer, primary_key=True)
+        comment_text = Column (Enum(MyEnum),nullable=False )
+        author_id = Column (Integer, ForeignKey ('User.id'))
+        post_id = Column (Integer, ForeignKey('Post.id'))
+
+        def to_dict(self):
+            return {}
+
+
+class Followers (Base):
+      __tablename__ = "Followers"
+      id = Column(Integer, primary_key=True)
+      user_from_id = Column (Integer, ForeignKey ('User.id'))
+      user_to_id = Column (Integer, ForeignKey ('User.id'))
+
+      def to_dict(self):
         return {}
+
 
 ## Draw from SQLAlchemy base
 try:
